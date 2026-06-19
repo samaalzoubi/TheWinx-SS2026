fb6af0dbf0f180de49a7e0d2ff7ecc7fb13bb4e7
Marianne Nosseir <marianne.wazif@gmail.com>
2026-06-19 15:06:05 +0200

    finish the payment service logic

---
+++ bc04-payment/src/main/java/com/winx/payment/dto/PaymentRequest.java
@@ -0,0 +1,56 @@
+package com.winx.payment.dto;
+
+import com.winx.payment.model.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public class PaymentRequest {
+    private Long bookingId;
+    private Long userId;
+    private BigDecimal amount;
+    private String currency;
+    private PaymentMethod paymentMethod;
+
+    public PaymentRequest() {
+    }
+
+    public Long getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(Long bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+}
---
+++ bc04-payment/src/main/java/com/winx/payment/repository/PaymentRepository.java
@@ -0,0 +1,11 @@
+package com.winx.payment.repository;
+
+import com.winx.payment.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    Optional<Payment> findByBookingId(Long bookingId);
+}
